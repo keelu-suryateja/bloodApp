475e73e91cfb8ca13b10484187fc12b881e97ec2
keelu-suryateja <keelusurya2003@gmail.com>
2026-05-25 11:39:41 +0530

    mail issue solved added port

--- app/mail/nodemailer.ts
+++ app/mail/nodemailer.ts
@@ -3,13 +3,14 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
 // ======================================================
 // PROFESSIONAL OTP EMAIL
 // ======================================================
